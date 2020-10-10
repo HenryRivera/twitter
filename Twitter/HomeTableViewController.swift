f2f942c5a85ad01cd3bb4a5525bd86de0086c249
Henry Rivera <henry.rivera@nyu.edu>
2020-10-09 22:56:43 -0400

    infinite scroll

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,11 +17,11 @@ class HomeTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
+        loadTweets()
         
         // target: Where do I want this to happen
         // Selector: What do you want us to do?
-        tweetRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        tweetRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         
         // telling table which refresh control to use
         tableView.refreshControl = tweetRefreshControl
@@ -34,9 +34,11 @@ class HomeTableViewController: UITableViewController {
     }
     
     
-    @objc func loadTweet(){
+    @objc func loadTweets(){
+        numTweets = 24
+        
         let baseURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let params = ["count": 24]
+        let params = ["count": numTweets]
         
         TwitterAPICaller.client?.getDictionariesRequest(url: baseURL, parameters: params, success: { (tweets: [NSDictionary]) in
             self.tweetArray.removeAll()
@@ -48,6 +50,29 @@ class HomeTableViewController: UITableViewController {
         }, failure: {(Error) in
             print("Could not retrieve tweets! Try again")
         })
+    }
+    
+    func loadMoreTweets(){
+        let baseURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numTweets += 24
+        let params = ["count": numTweets]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: baseURL, parameters: params, success: { (tweets: [NSDictionary]) in
+            self.tweetArray.removeAll()
+            for tweet in tweets{
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+        }, failure: {(Error) in
+            print("Could not retrieve tweets! Try again")
+        })
+    }
+    
+    // syntax for what we want to happen when user gets towards end of page
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath){
+        if indexPath.row + 1 == tweetArray.count{
+            loadMoreTweets()
+        }
     }
     
 
@@ -89,60 +114,5 @@ class HomeTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return tweetArray.count
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
