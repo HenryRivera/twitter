c570d983ab548db72e82dec59b7a36d51fad2614
Henry Rivera <henry.rivera@nyu.edu>
2020-10-10 00:27:38 -0400

    removing empty folder

---
+++ twitterTests/twitterTests.swift
@@ -0,0 +1,34 @@
+//
+//  twitterTests.swift
+//  twitterTests
+//
+//  Created by Henry Rivera on 10/10/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import XCTest
+@testable import twitter
+
+class twitterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
